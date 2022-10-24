1e4ed4c1a0ed9861eee62385ea17538e8d794872
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-10-24 21:10:58 +0700

    Add Makanan Propeties Config

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include "dataStructure/boolean.h"
 #include "dataStructure/Matrix/matrix.c"
+#include "dataStructure/ListStatik/liststatik.c"
 #include "procedure/command.c"
 #include "procedure/splashScreen.c"
 
 int main () {
     boolean isStarted = false, isExit = false;
-    Matrix map;
+    Matrix mapconfig;
+    ListStatik makananconfig;
     
     welcomePage();
-    inputCommand(&isStarted, &isExit, &map);
-    displayMatrix(map);
+    inputCommand(&isStarted, &isExit, &mapconfig, &makananconfig);
     while (!isExit) {
-        inputCommand(&isStarted, &isExit, &map);
+        inputCommand(&isStarted, &isExit, &mapconfig, &makananconfig);
     }
 
     
