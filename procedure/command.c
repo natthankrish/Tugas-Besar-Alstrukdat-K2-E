5c3e5f0e718f884ca0cbbed7fb41db8f1310e7dd
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-10-24 21:13:01 +0700

    add loadMakanan

--- procedure/command.c
+++ procedure/command.c
@@ -5,14 +5,19 @@
 #include "loadConfig.c"
 
 
-void inputCommand (boolean *isStarted, boolean *isExit, Matrix *peta) {
+void inputCommand (boolean *isStarted, boolean *isExit, Matrix *peta, ListStatik *makanan) {
     printf("Enter Command: ");
     STARTWORD();
 
     if (compareString(currentWord.TabWord, currentWord.Length, "START", 5)) {
-        *isStarted = true;
-        printf("LOADING...\n");
-        loadConfig(peta);
+        if (*isStarted) {
+            printf("Program sudah berjalan, silahkan lakukan command yang lain\n");
+            inputCommand(isStarted, isExit, peta, makanan);
+        } else {
+            *isStarted = true;
+            printf("LOADING...\n");
+            loadConfig(peta, makanan);
+        }
     } else if (compareString(currentWord.TabWord, currentWord.Length, "EXIT", 4)) {
         *isExit = true;
         printf("TERIMA KASIH TELAH MENGGUNAKAN BNMO, SAMPAI JUMPA KEMBALI!\n");
--- procedure/loadConfig.c
+++ procedure/loadConfig.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include "../dataStructure/CharMachine/charmachinefile.c"
+#include "../dataStructure/WordMachine/wordmachinefile.c"
 #include "../function/loadMakanan.c"
 #include "../function/loadResep.c"
 #include "../function/loadPeta.c"
 
-void loadConfig (Matrix *peta) {
+
+void loadConfig (Matrix *peta, ListStatik *makanan) {
     *peta = loadPeta();
+    *makanan = loadMakanan();
 }
