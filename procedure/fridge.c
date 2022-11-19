510be259a4688bab3ba02a8c35e65c92327e1c0b
farhanfadillahr <13519204@std.stei.itb.ac.id>
2022-11-19 15:41:13 +0700

    fix fridge walau belum fix banget

--- procedure/fridge.c
+++ procedure/fridge.c
@@ -8,6 +8,7 @@
 // Untuk cara memasukkan dibebaskan kepada kelompok masing-masing, pastikan dijelaskan saat demo.
 #include "../dataStructure/Inventory/inventory.h"
 #include "../dataStructure/Kulkas/kulkas.h"
+#include "../dataStructure/Makanan/makanan.h"
 
 void fridge(Inventory I, KULKAS *Kulkas){
     printf("===============================================\n");
@@ -23,11 +24,11 @@ void fridge(Inventory I, KULKAS *Kulkas){
     printf("\n");
     boolean keluar = false;
     while (!compareString(currentWord.TabWord, currentWord.Length, "0", 1) && !keluar) {
-        if(compareString(currentWord.TabWord, currentWord.Length, "1", 1)){
+        if(compareString(currentWord.TabWord, currentWord.Length, "1", 1) && currentChar == MARK){
             printf("Ini adalah Lihat Fridge\n");
             displayFridge(*Kulkas);
             keluar = true;
-        }else if(compareString(currentWord.TabWord, currentWord.Length, "2", 1)){
+        }else if(compareString(currentWord.TabWord, currentWord.Length, "2", 1) && currentChar == MARK){
             if(InventoryKosong(I)){
                 printf("Inventory kosong!\n");
                 keluar = true;
@@ -37,19 +38,21 @@ void fridge(Inventory I, KULKAS *Kulkas){
                 STARTWORD();
                 printf("\n");
                 for(int i = 0; i < NBElmt(I);i++){
+                    int idx = (i + Head(I))%NBElmt(I);
                     int order = i + 1;
                     char orderString[2];
                     sprintf(orderString, "%d", order);
                     if (compareString(currentWord.TabWord, currentWord.Length,  orderString, countIntegerLength(order)) && currentChar == MARK){
-                        printf("keyboard:%c listinv:%d\n",currentWord.TabWord, i);
-                        if(MakananDalamInventory(I, Elmt(I,i))){
-                            inToFridge(Kulkas, &Elmt(I,i));
-                            // for(int j = 0; j < 101; j++){
-                            //     printf("%c",Elmt(I,i).name[j]);
-                            // }
-                            AmbilMakanan(&I, Elmt(I,i));
+                        // printf("keyboard:%c listinv:%d\n",currentWord.TabWord, i);
+                        if(MakananDalamInventory(I, Elmt(I,idx))){
+                            inToFridge(Kulkas, &Elmt(I,idx));
+                            for(int j = 0; j < 101; j++){
+                                printf("%c",Elmt(I,idx).name[j]);
+                            }
+                            AmbilMakanan(&I, Elmt(I,idx));
                             displayFridge(*Kulkas);
-                            // PrintInventory((I));
+                            PrintInventory((I));
+                            I = I;
                             
                             // delElmtInventory(I, i);
                         }
@@ -57,9 +60,9 @@ void fridge(Inventory I, KULKAS *Kulkas){
                 }
                 keluar = true;
             }
-        }else if(compareString(currentWord.TabWord, currentWord.Length, "3", 1)){
+        }else if(compareString(currentWord.TabWord, currentWord.Length, "3", 1) && currentChar == MARK){
             if(isFridgeEmpty(*Kulkas)){
-                // printf("Inventory kosong!\n");
+                printf("Inventory kosong!\n");
                 keluar = true;
             }else{
                 displayFoodList(*Kulkas);
@@ -68,16 +71,20 @@ void fridge(Inventory I, KULKAS *Kulkas){
                 printf("\n");
                 for(int i = 0; i < FridgeListLength(*Kulkas);i++){
                     int order = i + 1;
+                    int idx = (i + Head(I))%NBElmt(I);
                     char orderString[2];
                     sprintf(orderString, "%d", order);
                     if (compareString(currentWord.TabWord, currentWord.Length,  orderString, countIntegerLength(order)) && currentChar == MARK){
-                        printf("keyboard:%c listinv:%d\n",currentWord.TabWord, i);
+                        // printf("keyboard:%c listinv:%d\n",currentWord.TabWord, i);
                         printf("%s", Name(FoodListELMT(*Kulkas,i)));
+                        outFromFridge(Kulkas, i, &FoodListELMT(*Kulkas,i));
+                        InsertMakanan(&I, FoodListELMT(*Kulkas,i));
                         // outFromFridge(Kulkas, &Elmt(Kulkas->foodList,i));
                     }
                 }
                 keluar = true;
             }
+            keluar = true;
         }else{
             while (!endWord) {
                 ADVWORD();
