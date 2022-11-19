a83b6b47d41078c39b23fbaf1073db004a9bd87d
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-11-19 18:25:21 +0700

    add: closePage splashScreen

--- procedure/splashScreen.c
+++ procedure/splashScreen.c
@@ -39,3 +39,17 @@ void welcomePage () {
     "SELAMAT DATANG DI SIMULASI MEMASAK BNMO"
     );
 }
+
+void closePage () {
+    puts("\n"
+    "        88                                 88\n"                                            
+"  ,d    88                                 88\n"                                            
+"  88    88                                 88\n"                                            
+"MM88MMM 88,dPPYba,  ,adPPYYba, 8b,dPPYba,  88   ,d8 8b       d8  ,adPPYba,  88       88\n"  
+"  88    88P'    \"8a \"\"     `Y8 88P'   `\"8a 88 ,a8\"  `8b     d8' a8\"     \"8a 88       88\n"  
+"  88    88       88 ,adPPPPP88 88       88 8888[     `8b   d8'  8b       d8 88       88\n"  
+"  88,   88       88 88,    ,88 88       88 88`\"Yba,   `8b,d8'   \"8a,   ,a8\" \"8a,   ,a88\n"  
+"  \"Y888 88       88 `\"8bbdP\"Y8 88       88 88   `Y8a    Y88'     `\"YbbdP\"'   `\"YbbdP'Y8\n"  
+"                                                        d8'\n"                              
+"                                                       d8'\n\n");           
+}
