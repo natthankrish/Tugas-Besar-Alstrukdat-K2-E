f27777962f300a57ae849b00422068e380f821f1
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2022-11-20 10:35:02 +0700

    benerin print delivery

--- procedure/delivery.c
+++ procedure/delivery.c
@@ -9,19 +9,19 @@ void delivery(PrioQueue pesanan){
         for(int i = 0; i < NBElmt(pesanan);i++){
             printf("    %d. ",i+1);
             for(int j = 0; j < 101; j++){
-                printf("%c",Elmt(pesanan,i).name[j]);
+                printf("%c",Elmt(pesanan,(i+Head(pesanan))%MaxEl(pesanan)).name[j]);
             }
             printf("\n      Waktu sisa delivery : ");
-            if (Elmt(pesanan,i).deliveryTime.D != 0) {
-                printf(" %d Hari", Elmt(pesanan,i).deliveryTime.D);
+            if (Elmt(pesanan,(i+Head(pesanan))%MaxEl(pesanan)).deliveryTime.D != 0) {
+                printf(" %d Hari", Elmt(pesanan,(i+Head(pesanan))%MaxEl(pesanan)).deliveryTime.D);
             }
 
-            if (Elmt(pesanan,i).deliveryTime.HH != 0) {
-                printf(" %d Jam", Elmt(pesanan,i).deliveryTime.HH);
+            if (Elmt(pesanan,(i+Head(pesanan))%MaxEl(pesanan)).deliveryTime.HH != 0) {
+                printf(" %d Jam", Elmt(pesanan,(i+Head(pesanan))%MaxEl(pesanan)).deliveryTime.HH);
             }
 
-            if (Elmt(pesanan,i).deliveryTime.MM != 0) {
-                printf(" %d Menit", Elmt(pesanan,i).deliveryTime.MM);
+            if (Elmt(pesanan,(i+Head(pesanan))%MaxEl(pesanan)).deliveryTime.MM != 0) {
+                printf(" %d Menit", Elmt(pesanan,(i+Head(pesanan))%MaxEl(pesanan)).deliveryTime.MM);
             }
             printf("\n\n");
         }
